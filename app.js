a15d5a9a2099dd10316ea6ef6d14d89864afad3a
eshginfarzali <85007480+eshginfarzali@users.noreply.github.com>
2023-01-18 20:23:36 +0400

    optimaltion

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let order=prompt('Sifarsi zehmet olmasa bu formada daxil edin: kfc/fri/2', 'kfc/fri/2')
+
+/*let order=prompt('Sifarsi zehmet olmasa bu formada daxil edin: kfc/fri/2', 'kfc/fri/2')
  
 let arr= order.toLocaleLowerCase().split('/')
 
@@ -23,6 +24,35 @@ else{
 else{
 console.log("Yanlis sorgu, zehmet olmasa sifarsi yeniden daxil edrin")
 }
+
+}
+
+delivery()
+*/
+function delivery(){
+    let order=prompt('Sifarsi zehmet olmasa bu formada daxil edin: kfc/fri/2', 'kfc/fri/2')
+ 
+let arr= order.toLocaleLowerCase().split('/')
+
+
+if((arr[0]==='mcdonalds' || arr[0]==='kfc' || arr[0]==='burger king')&&  (arr[1]==='fri'|| arr[1]==='burger' || arr[1]==='twister') &&  +arr[2]>=2) {
+ if (confirm('Restoren:Sifarisi qebul edirsiniz?')) {
+    if(confirm('Kuryer: Sifarsi qebul edirsiniz?')){
+        console.log('Sifarisiniz hazirdir!')
+    }
+    else{
+        console.log('Sifaris kuryer tapilmadi!')
+    }
+
+}
+else{
+    console.log('Restoran sifarisi legv etdi!')
+}
+
+}
+else{
+console.log("Yanlis sorgu, zehmet olmasa sifarsi yeniden daxil edrin")
+ }
 
 }
 
