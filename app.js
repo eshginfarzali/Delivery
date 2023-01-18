a830f2f0749297da10fdfa99f1433554ebc00ccd
eshginfarzali <85007480+eshginfarzali@users.noreply.github.com>
2023-01-18 19:47:55 +0400

    first-commit

---
+++ app.js
@@ -0,0 +1,29 @@
+let order=prompt('Sifarsi zehmet olmasa bu formada daxil edin: kfc/fri/2', 'kfc/fri/2')
+ 
+let arr= order.toLocaleLowerCase().split('/')
+console.log(arr)
+
+function delivery(product, restuartn, money){
+
+if((arr[0]==='mcdonalds' || arr[0]==='kfc' || arr[0]==='burger king')&&  (arr[1]==='fri'|| arr[1]==='burger' || arr[1]==='twister') &&  +arr[2]>=2) {
+ if (confirm('Restoren:Sifarisi qebul edirsiniz?') == true) {
+    if(confirm('Kuryer: Sifarsi qebul edirsiniz?')==true){
+        console.log('Sifarisiniz hazirdir!')
+    }
+    else{
+        console.log('Sifaris kuryer tapilmadi!')
+    }
+
+}
+else{
+    console.log('Restoran sifarisi legv etdi!')
+}
+
+}
+else{
+console.log("Yanlis sorgu, zehmet olmasa sifarsi yeniden daxil edrin")
+}
+
+}
+
+delivery()
